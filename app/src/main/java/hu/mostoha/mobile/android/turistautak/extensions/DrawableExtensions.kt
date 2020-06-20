b68436b5eff39faa20b5b2bc6f2f0bb564bfa74a
Mostoha, Roland <mostoha.roland@autsoft.hu>
2020-06-20 23:33:43 +0200

    feat(Map): add layers FAB, add ProgressFloatingActionButton

--- app/src/main/java/hu/mostoha/mobile/android/turistautak/extensions/DrawableExtensions.kt
+++ app/src/main/java/hu/mostoha/mobile/android/turistautak/extensions/DrawableExtensions.kt
@@ -2,21 +2,10 @@ package hu.mostoha.mobile.android.turistautak.extensions
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
-
+import androidx.core.graphics.drawable.toBitmap
 
 fun @receiver:DrawableRes Int.toBitmap(context: Context): Bitmap {
-    val drawable = ContextCompat.getDrawable(context, this)!!
-
-    val bitmap = Bitmap.createBitmap(
-        drawable.intrinsicWidth,
-        drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
-    )
-    val canvas = Canvas(bitmap)
-    drawable.setBounds(0, 0, canvas.width, canvas.height)
-    drawable.draw(canvas)
-
-    return bitmap
+    return ContextCompat.getDrawable(context, this)!!.toBitmap()
 }
---
+++ app/src/main/java/hu/mostoha/mobile/android/turistautak/views/ProgressFloatingActionButton.kt
@@ -0,0 +1,46 @@
+package hu.mostoha.mobile.android.turistautak.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import hu.mostoha.mobile.android.turistautak.R
+import kotlinx.android.synthetic.main.view_progress_floating_action_button.view.*
+
+class ProgressFloatingActionButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        LayoutInflater
+            .from(context)
+            .inflate(R.layout.view_progress_floating_action_button, this, true)
+
+        with(context.obtainStyledAttributes(attrs, R.styleable.ProgressFloatingActionButton)) {
+            fab.setImageDrawable(getDrawable(R.styleable.ProgressFloatingActionButton_iconSrc))
+
+            val fabSize = getInt(
+                R.styleable.ProgressFloatingActionButton_fabSize,
+                FloatingActionButton.SIZE_AUTO
+            )
+            fab.size = fabSize
+
+            val progressBarSize = resources.getDimensionPixelSize(
+                when (fabSize) {
+                    FloatingActionButton.SIZE_MINI -> R.dimen.progress_fab_progress_bar_size_mini
+                    FloatingActionButton.SIZE_NORMAL -> R.dimen.progress_fab_progress_bar_size_normal
+                    else -> 0
+                }
+            )
+            progressBar.layoutParams.apply {
+                height = progressBarSize
+                width = progressBarSize
+            }
+
+            recycle()
+        }
+
+
+    }
+}
