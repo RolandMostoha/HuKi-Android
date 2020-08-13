f9c85495eb6f0d9ba3de5a405ad9e8450978df1d
Roland Mostoha <roland.mostoha@gmail.com>
2020-08-13 17:32:40 +0200

    design(SearchBar): add dedicated icons for node, way, relation

--- app/src/main/java/hu/mostoha/mobile/android/turistautak/ui/home/HomeUiModelGenerator.kt
+++ app/src/main/java/hu/mostoha/mobile/android/turistautak/ui/home/HomeUiModelGenerator.kt
@@ -3,6 +3,7 @@ package hu.mostoha.mobile.android.turistautak.ui.home
 import hu.mostoha.mobile.android.turistautak.R
 import hu.mostoha.mobile.android.turistautak.model.domain.PlaceDetails
 import hu.mostoha.mobile.android.turistautak.model.domain.PlacePrediction
+import hu.mostoha.mobile.android.turistautak.model.domain.PlaceType
 import hu.mostoha.mobile.android.turistautak.model.domain.toGeoPoint
 import hu.mostoha.mobile.android.turistautak.model.ui.PlaceDetailsUiModel
 import hu.mostoha.mobile.android.turistautak.model.ui.PlacePredictionUiModel
@@ -17,7 +18,11 @@ class HomeUiModelGenerator @Inject constructor() {
                 placeType = it.placeType,
                 primaryText = it.primaryText,
                 secondaryText = it.secondaryText,
-                iconRes = R.drawable.ic_home_search_bar_type_node
+                iconRes = when (it.placeType) {
+                    PlaceType.NODE -> R.drawable.ic_home_search_bar_type_node
+                    PlaceType.WAY -> R.drawable.ic_home_search_bar_type_way
+                    PlaceType.RELATION -> R.drawable.ic_home_search_bar_type_relation
+                }
             )
         }
     }
