f05994fa31b9a737c5e06f803a580ecc0041fabc
Roland Mostoha <roland.mostoha@gmail.com>
2021-09-10 12:08:13 +0200

    fix(Photon): update photon url to photon.komoot.io

--- app/src/main/java/hu/mostoha/mobile/android/huki/network/NetworkConfig.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/network/NetworkConfig.kt
@@ -3,7 +3,7 @@ package hu.mostoha.mobile.android.huki.network
 
 object NetworkConfig {
     const val BASE_URL_OVERPASS = "https://overpass-api.de"
-    const val BASE_URL_PHOTON = "https://photon.komoot.de"
+    const val BASE_URL_PHOTON = "https://photon.komoot.io"
     const val TIMEOUT_SEC = 30
     const val SEARCH_QUERY_DELAY_MS = 800L
 }
