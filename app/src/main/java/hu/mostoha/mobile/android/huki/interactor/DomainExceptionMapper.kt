13e09f5700c6a4152f92b419cc9ab14aea70f037
Roland Mostoha <roland.mostoha@gmail.com>
2022-06-13 18:06:00 +0200

    feat(ErrorHandling): handle HTTP-504 Gateway Timeout errors

--- app/src/main/java/hu/mostoha/mobile/android/huki/interactor/DomainExceptionMapper.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/interactor/DomainExceptionMapper.kt
@@ -1,20 +1,21 @@
 package hu.mostoha.mobile.android.huki.interactor
 
-import hu.mostoha.mobile.android.huki.interactor.exception.DomainException
-import hu.mostoha.mobile.android.huki.interactor.exception.HikingLayerFileDownloadFailedException
-import hu.mostoha.mobile.android.huki.interactor.exception.TooManyRequestsException
-import hu.mostoha.mobile.android.huki.interactor.exception.UnknownException
+import hu.mostoha.mobile.android.huki.interactor.exception.*
 import retrofit2.HttpException
 import java.io.FileNotFoundException
 
 object DomainExceptionMapper {
 
     private const val HTTP_CODE_TOO_MANY_REQUESTS = 429
+    private const val HTTP_CODE_GATEWAY_TIMEOUT = 504
 
     fun map(exception: Exception): DomainException {
         return when {
             exception is HttpException && exception.code() == HTTP_CODE_TOO_MANY_REQUESTS -> {
                 TooManyRequestsException(exception)
+            }
+            exception is HttpException && exception.code() == HTTP_CODE_GATEWAY_TIMEOUT -> {
+                GatewayTimeoutException(exception)
             }
             exception is FileNotFoundException -> {
                 HikingLayerFileDownloadFailedException(exception)
--- app/src/test/java/hu/mostoha/mobile/android/huki/interactor/DomainExceptionMapperTest.kt
+++ app/src/test/java/hu/mostoha/mobile/android/huki/interactor/DomainExceptionMapperTest.kt
@@ -2,6 +2,7 @@ package hu.mostoha.mobile.android.huki.interactor
 
 import com.google.common.truth.Truth.assertThat
 import hu.mostoha.mobile.android.huki.R
+import hu.mostoha.mobile.android.huki.interactor.exception.GatewayTimeoutException
 import hu.mostoha.mobile.android.huki.interactor.exception.HikingLayerFileDownloadFailedException
 import hu.mostoha.mobile.android.huki.interactor.exception.TooManyRequestsException
 import hu.mostoha.mobile.android.huki.interactor.exception.UnknownException
@@ -22,6 +23,16 @@ class DomainExceptionMapperTest {
 
         assertThat(mappedException).isEqualTo(TooManyRequestsException(exception))
         assertThat(mappedException.messageRes).isEqualTo(R.string.error_message_too_many_requests.toMessage())
+    }
+
+    @Test
+    fun `Given HTTP 504 Gateway Timeout exception, when map, then GatewayTimeoutException returns`() {
+        val exception = HttpException(Response.error<Unit>(504, "".toResponseBody()))
+
+        val mappedException = DomainExceptionMapper.map(exception)
+
+        assertThat(mappedException).isEqualTo(GatewayTimeoutException(exception))
+        assertThat(mappedException.messageRes).isEqualTo(R.string.error_message_gateway_timeout.toMessage())
     }
 
     @Test
