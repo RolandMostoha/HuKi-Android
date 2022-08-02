ebfb804416b237a6c10d8eb47405b433aa0c34ed
Roland Mostoha <roland.mostoha@gmail.com>
2022-08-02 09:08:17 +0200

    fix(Directions): remove forcing Google Maps direction intent package

--- app/src/main/java/hu/mostoha/mobile/android/huki/util/MapUtils.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/util/MapUtils.kt
@@ -28,8 +28,6 @@ val HUNGARY_BOUNDING_BOX = BoundingBox(
 fun AppCompatActivity.startGoogleDirections(geoPoint: GeoPoint) {
     val latLng = "${geoPoint.latitude},${geoPoint.longitude}"
     val uri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=$latLng")
-    val mapIntent = Intent(Intent.ACTION_VIEW, uri).apply {
-        setPackage("com.google.android.apps.maps")
-    }
-    startActivity(mapIntent)
+
+    startActivity(Intent(Intent.ACTION_VIEW, uri))
 }
