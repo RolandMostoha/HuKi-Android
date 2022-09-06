8ddf14db1fd9832e68ad13709cb7a72b326dd29c
Roland Mostoha <roland.mostoha@gmail.com>
2022-09-06 07:09:03 +0200

    feat(MapnikTileSource): use new Mapnik without multiple URLs

--- app/src/main/java/hu/mostoha/mobile/android/huki/model/domain/BaseLayer.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/model/domain/BaseLayer.kt
@@ -1,11 +1,12 @@
 package hu.mostoha.mobile.android.huki.model.domain
 
+import hu.mostoha.mobile.android.huki.osmdroid.tilesource.MapnikTileSource
 import org.osmdroid.tileprovider.tilesource.ITileSource
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
 sealed class BaseLayer(layerType: LayerType, tileSource: ITileSource) : LayerSpec(layerType, tileSource) {
 
-    object Mapnik : BaseLayer(LayerType.MAPNIK, TileSourceFactory.MAPNIK)
+    object Mapnik : BaseLayer(LayerType.MAPNIK, MapnikTileSource)
 
     object OpenTopo : BaseLayer(LayerType.OPEN_TOPO, TileSourceFactory.OpenTopo)
 
