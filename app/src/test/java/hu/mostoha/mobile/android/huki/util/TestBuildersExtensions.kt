edba4b36da7782ebb68f229fca7ab002255a3c41
Roland Mostoha <roland.mostoha@gmail.com>
2024-10-08 07:53:55 +0200

    fix(GpxLib): use aliyun maven repo instead of jcenter

--- app/src/test/java/hu/mostoha/mobile/android/huki/util/TestBuildersExtensions.kt
+++ app/src/test/java/hu/mostoha/mobile/android/huki/util/TestBuildersExtensions.kt
@@ -1,12 +1,10 @@
 package hu.mostoha.mobile.android.huki.util
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-@OptIn(ExperimentalCoroutinesApi::class)
 fun runTestDefault(
     context: CoroutineContext = EmptyCoroutineContext,
     dispatchTimeoutMs: Long = 1000,
