db3cecd0cbec2eaa9b56bc698041faf457f1941a
Roland Mostoha <roland.mostoha@gmail.com>
2024-10-10 07:22:21 +0200

    chore(OKT): update OKT stamp waypoint and locations

--- app/src/main/java/hu/mostoha/mobile/android/huki/repository/OktRepository.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/repository/OktRepository.kt
@@ -5,7 +5,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import hu.mostoha.mobile.android.huki.R
 import hu.mostoha.mobile.android.huki.model.domain.Location
 import hu.mostoha.mobile.android.huki.model.domain.OktRoutes
-import hu.mostoha.mobile.android.huki.model.domain.OktStampWaypoint
 import hu.mostoha.mobile.android.huki.model.mapper.OktRoutesMapper
 import io.ticofab.androidgpxparser.parser.GPXParser
 import javax.inject.Inject
@@ -16,38 +15,24 @@ class OktRepository @Inject constructor(
 ) {
 
     fun getOktRoutes(): OktRoutes {
-        return OktRoutes(
-            locations = getOktFullRoute(),
-            stampWaypoints = getOktStampWaypoints(),
-        )
-    }
-
-    private fun getOktFullRoute(): List<Location> {
-        val inputStream = context.resources.openRawResource(R.raw.okt_full)
+        val inputStream = context.resources.openRawResource(R.raw.okt_teljes_bh_20240923)
 
         val gpx = GPXParser().parse(inputStream)
 
-        return gpx.tracks
+        val gpxWaypoints = gpx.wayPoints
+        val stampWaypoints = oktRoutesMapper.map(gpxWaypoints)
+        check(gpxWaypoints.count() == stampWaypoints.map { it.stampNumber }.count()) {
+            "GPX waypoints count should match with stamp numbers count"
+        }
+
+        val locations = gpx.tracks
             .flatMap { it.trackSegments }
             .flatMap { it.trackPoints }
             .map { trackPoint ->
                 Location(trackPoint.latitude, trackPoint.longitude, trackPoint.elevation)
             }
-    }
 
-    private fun getOktStampWaypoints(): List<OktStampWaypoint> {
-        val inputStream = context.resources.openRawResource(R.raw.okt_stamp_locations)
-
-        val gpx = GPXParser().parse(inputStream)
-        val gpxWaypoints = gpx.wayPoints
-
-        val waypoints = oktRoutesMapper.map(gpxWaypoints)
-
-        check(gpxWaypoints.count() == waypoints.map { it.stampNumber }.count()) {
-            "GPX waypoints count should match with stamp numbers count"
-        }
-
-        return waypoints
+        return OktRoutes(locations, stampWaypoints)
     }
 
 }
