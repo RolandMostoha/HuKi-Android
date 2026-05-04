694ae04035be5cf3015db06a5b81072bc0615f50
Roland Mostoha <roland.mostoha@zenitech.co.uk>
2026-05-04 16:01:40 +0200

    feat(Layers): remove Amplify init from HukiApplication

--- app/src/main/java/hu/mostoha/mobile/android/huki/HukiApplication.kt
+++ app/src/main/java/hu/mostoha/mobile/android/huki/HukiApplication.kt
@@ -1,12 +1,6 @@
 package hu.mostoha.mobile.android.huki
 
 import android.app.Application
-import com.amplifyframework.AmplifyException
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
-import com.amplifyframework.auth.options.AuthFetchSessionOptions
-import com.amplifyframework.core.Amplify
-import com.amplifyframework.core.AmplifyConfiguration
-import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
@@ -32,45 +26,12 @@ class HukiApplication : Application() {
         osmConfiguration.init()
 
         initAnalytics()
-        initAmplify()
         initTimber()
     }
 
     private fun initAnalytics() {
         FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(isRelease)
         FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(isRelease)
-    }
-
-    private fun initAmplify() {
-        try {
-            val config = AmplifyConfiguration
-                .builder(applicationContext)
-                .devMenuEnabled(false)
-                .build()
-            Amplify.addPlugin(AWSCognitoAuthPlugin())
-            Amplify.addPlugin(AWSS3StoragePlugin())
-            Amplify.configure(config, applicationContext)
-
-            forceRefreshToken()
-        } catch (exception: AmplifyException) {
-            Timber.e(exception)
-
-            exceptionLogger.recordException(exception)
-        }
-    }
-
-    private fun forceRefreshToken() {
-        Amplify.Auth.fetchAuthSession(
-            AuthFetchSessionOptions.builder()
-                .forceRefresh(true)
-                .build(),
-            { _ -> },
-            { authException ->
-                Timber.e(authException, "Amplify.Auth failed to fetch auth session")
-
-                exceptionLogger.recordException(authException)
-            }
-        )
     }
 
     private fun initTimber() {
